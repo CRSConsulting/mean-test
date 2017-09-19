c2f3a47db9ee11a1aece29b890a1c4fbd17d2700
John <John@crs.consulting.com>
2017-09-19 12:49:24 -0700

    env issues with mongodb

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 console.log('++++++++++++++++++++++++++++++++++++++++++++')
 // const mongoUri = process.env.MONGODB_URI
-const mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`
+// const mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
+const mongoUri = 'mongodb://username:password@ds151973.mlab.com:51973/738356255801'
 function connect() {
  mongoose.set('debug', true);
  return mongoose.connect(mongoUri, { useMongoClient: true });
