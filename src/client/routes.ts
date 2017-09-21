1608f118afeef858062a4b08ecd9e930aa74b5ab
John <John@crs.consulting.com>
2017-09-21 12:46:24 -0700

    added client side routes

---
+++ src/client/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
+export const appRoutes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'loaderio-6f795de852dfe0866bc1f8d54bc851d0/', component: AppComponent }
+]
