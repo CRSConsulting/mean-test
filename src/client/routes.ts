8d85c4e22c9e5bd5c95ce7b0a27007a72a10fdd1
John <John@crs.consulting.com>
2017-09-21 14:03:34 -0700

    adding loader.io

--- src/client/routes.ts
+++ src/client/routes.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app/app.component';
 import { MobilesComponent } from './app/mobiles.component';
 
 export const appRoutes: Routes = [
-  { path: 'loaderio-6f795de852dfe0866bc1f8d54bc851d0', component: AppComponent },
+  { path: 'loaderio-6f795de852dfe0866bc1f8d54bc851d0.txt', component: AppComponent },
   { path: 'mobile',      component: MobilesComponent },
 
   { path: '',
